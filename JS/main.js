e860852463907ee49ea35b534264b9626ef76922
Guildyn <guildyn@Adilsons-MacBook-Pro.local>
2024-09-07 22:34:28 +0100

    fixed event listener.

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 // Smooth scrolling for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', e => {
+    anchor.addEventListener('click', function(e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
@@ -9,7 +9,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 // Contact form submission
-document.getElementById('contact-form').addEventListener('submit', e => {
+document.getElementById('contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
     alert("Your message has been sent!");
     // TODO: add AJAX call to send the form data to a server
